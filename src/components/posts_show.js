64ad53bd562cb18f0f10aee2622409cb74511a9f
Phan Thanh <thanhpd56@gmail.com>
2018-02-21 15:19:16 +0700

    fix: bind function mistake

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -17,7 +17,7 @@ class PostsShow extends React.Component {
         }
         return <div>
             <Link to="/">{"<<"}Back to home</Link>
-            <button className="btn btn-danger pull-xs-right" onClick={this.onDeleteClick()}>Delete</button>
+            <button className="btn btn-danger pull-xs-right" onClick={ () => {this.onDeleteClick()}} >Delete</button>
             <h3>{post.title}</h3>
             <h6>Categories: {post.categories}</h6>
             <p>{post.content}</p>
